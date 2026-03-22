e94c9a482ba2091dbc76793ab859e836a7efb20a
Chen Jiajun <c.jiajun.92@gmail.com>
2026-03-22 21:00:11 +0800

    use shell sript for terraform init

---
+++ terraform/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Initializing Terraform with GitLab Managed State..."
+
+terraform init \
+  -backend-config="address=${TF_ADDRESS}" \
+  -backend-config="lock_address=${TF_ADDRESS}/lock" \
+  -backend-config="unlock_address=${TF_ADDRESS}/lock" \
+  -backend-config="username=gitlab-ci-token" \
+  -backend-config="password=${CI_JOB_TOKEN}" \
+  -backend-config="lock_method=POST" \
+  -backend-config="unlock_method=DELETE" \
+  -backend-config="retry_wait_min=5"
