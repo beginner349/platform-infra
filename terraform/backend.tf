30877dd49367a7940b1753341caffbb06d5c4dd1
Chen Jiajun <c.jiajun.92@gmail.com>
2026-05-03 20:30:17 +0800

    test workflow for terraform deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "http" {
-
+  backend "s3" {
+    bucket       = "terraform-state-bucket-542776678091-ap-southeast-1-an"
+    key          = "terraform/state/platform-infra.tfstate"
+    region       = "ap-southeast-1" # Matches your existing configuration [5]
+    use_lockfile = true             # Enables native S3 state locking [3]
   }
 }
